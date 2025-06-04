4cad26006b1353ba6d2c292300aa8658aa8e3f27
mateo-stay <mate.estay@duocuc.cl>
2025-06-03 22:22:32 -0400

    cambios

--- biblioteca.salas.duoc/src/main/java/biblioteca/salas/douc/biblioteca/salas/duoc/config/SwaggerConfig.java
+++ biblioteca.salas.duoc/src/main/java/biblioteca/salas/douc/biblioteca/salas/duoc/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package biblioteca.salas.douc.biblioteca.salas.duoc.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +10,25 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI bibliotecaOpenAPI() {
         return new OpenAPI()
             .info(new Info()
-                .title("Proyecto Biblioteca Salas Duoc API")
-                .version("1.0.0")
-                .description("Documentación de la API para reservas de salas"));
+                .title("📚 API Biblioteca Salas Duoc")
+                .version("v1.0.0")
+                .description("API REST para gestión de reservas, estudiantes, salas y carreras en Biblioteca Duoc"));
+    }
+
+    @Bean
+    public GlobalOpenApiCustomizer agregarHeaderPorDefecto() {
+        return openApi -> {
+            openApi.getPaths()
+                .values()
+                .forEach(pathItem -> pathItem.readOperations()
+                    .forEach(op -> op.addParametersItem(new io.swagger.v3.oas.models.parameters.Parameter()
+                        .name("X-Cliente-Id")
+                        .description("Identificador único de la entidad cliente")
+                        .required(false)
+                        .schema(new io.swagger.v3.oas.models.media.StringSchema()))));
+        };
     }
 }
